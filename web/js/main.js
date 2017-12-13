276b8c040739908f3916e8df6a8b5baf2704f2c1
hcpss-vincevince1006 <vincent_dubay@hcpss.org>
2017-12-13 15:55:34 -0500

    condensed ajax call
    Makes one call based on a single button click rather than two separate
    calls for two separate button click events.

--- web/js/main.js
+++ web/js/main.js
@@ -1,89 +1,52 @@
-$("button#dir-submit").click(function(e){
+$("button#submit").click(function(e){
   e.preventDefault();
   var $path = "";
+  var $mod = "";
+  var $q_string = "";
+    
   var $query = $("#search-field").val();
   var $render = $('#render-cards');
+
+  var $selected = $("#search-toggle option:selected").val();
+
+  if ($selected === "dept") {
+    $q_string = "given-dept=";
+  } else {
+    $q_string = "given-name=";
+  }
+
   $.ajax({
     type: "GET",
     url: $path,
-    data: 'given-name=' + $query,
+    data: $q_string + $query,
     dataType: "html",
     success: function(response){
       $render.hide().html(response).fadeIn();
     },
     complete: function() {
-      var $divAmt = $(".dir-search__entry").length;
-      var $resultCount = $("#result-output").hide().fadeIn();
-      var $helperText = "<p>Can't find who you're looking for? Try our full department listing or call our main phone line, <strong>410-313-6000</strong> during normal business hours, 8:30 a.m. - 4:30 p.m.</p>";
-        if ($query === "") {
-            $resultCount.html("<h3>Please enter in a full or partial name</h3>");
-        } else if ($divAmt < 1) {
-            $resultCount.html("<h3>No results found</h3>" + $helperText);
+    var $divAmt = $(".dir-search__entry").length;
+    var $resultCount = $("#result-output").hide().fadeIn();
+
+      if ($query === "") {
+        if ($q_string === "given-dept=") {
+          $resultCount.html("<h3>Please enter in a full or partial department name</h3>");
         } else {
-            if ($divAmt === 1){
-            $resultCount.html("<h3>" + $divAmt + " result found</h3>");
-            } else {
-            $resultCount.html("<h3>" + $divAmt + " results found</h3>");
-            }
-        };  
-      }
-  });
-});
-
-$("button#dept-submit").click(function(e){
-  e.preventDefault();
-  var $path = "";
-  var $query = $("#search-field2").val();
-  var $render = $('#render-cards');
-  $.ajax({
-    type: "GET",
-    url: $path,
-    data: 'given-dept=' + $query,
-    dataType: "html",
-    success: function(response){
-      $render.hide().html(response).fadeIn();
-    },
-    complete: function() {
-      var $divAmt = $(".dir-search__entry").length;
-      var $resultCount = $("#result-output").hide().fadeIn();
-      var $helperText = "<p>Can't find who you're looking for? Try our full department listing or call our main phone line, <strong>410-313-6000</strong> during normal business hours, 8:30 a.m. - 4:30 p.m.</p>";
-        if ($query === "") {
-            $resultCount.html("<h3>Please enter in a full or partial department name</h3>");
-        } else if ($divAmt < 1) {
-            $resultCount.html("<h3>No results found</h3>" + $helperText);
+          $resultCount.html("<h3>Please enter in a full or partial staff name</h3>"); 
+        }
+      } else if ($divAmt < 1) {
+        if ($q_string === "given-dept=") {
+          $mod = "what";
         } else {
-            if ($divAmt === 1){
-            $resultCount.html("<h3>" + $divAmt + " result found</h3>");
-            } else {
-            $resultCount.html("<h3>" + $divAmt + " results found</h3>");
-            }
-        };  
-      }
-  });
-});
-
-$("#dept-switch").click(function(e) {
-  e.preventDefault();
-  $(".dir-search__name--input").fadeOut("slow", function(){
-    var styles = {
-      position : "relative",
-      left : "0px"
+          $mod = "who";
+        }
+        $resultCount.html("<h3>No results found</h3>" + "<p>Can't find " + $mod + " you're looking for? Try our <a href='/directory/departments'>full department listing</a> or call our main phone line, <strong>410-313-6000</strong> during normal business hours, 8:30 a.m. - 4:30 p.m.</p>");
+      } else {
+        if ($divAmt === 1){
+          $resultCount.html("<h3>" + $divAmt + " result found</h3>");
+        } else {
+          $resultCount.html("<h3>" + $divAmt + " results found</h3>");
+        }
+      };  
     }
-    $(".dir-search__dept--input").css( styles ).fadeIn("slow");
   });
-  $('#render-cards').hide();
-  $('#result-output').hide();
-});
-
-$("#staff-switch").click(function(e) {
-  e.preventDefault();
-  $(".dir-search__dept--input").fadeOut("slow", function(){
-    var styles = {
-      position : "absolute",
-      left : "-9999px"
-    }
-    $(".dir-search__name--input").fadeIn("slow");
-  });
-  $('#render-cards').hide();
-  $('#result-output').hide();  
 });
