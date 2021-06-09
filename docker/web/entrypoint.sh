3939023df1bcab2822cc6a3145705a8e51671654
Brendan Anderson <brendan_anderson@hcpss.org>
2021-06-09 15:30:59 -0400

    Update permissions

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -6,5 +6,6 @@ chown -R www-data:www-data /var/www/symfony/public/images
 chown -R www-data:www-data /var/www/symfony/public/imagine
 
 /var/www/symfony/bin/console app:data:index
+chown -R www-data:www-data /var/www/symfony/var
 
 exec "$@"
